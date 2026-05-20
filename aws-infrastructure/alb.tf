87ec9f6b14515c72732b4e5dc790a34194341b40
Shoban Chiddarth <messageshoban@gmail.com>
2026-05-20 19:09:38 +0530

    Satisfied alb multi az requirement

--- aws-infrastructure/alb.tf
+++ aws-infrastructure/alb.tf
@@ -2,6 +2,6 @@ resource "aws_alb" "webserver_alb" {
   name = "webserver-alb"
   internal = false
   load_balancer_type = "application"
-  subnets = [ aws_subnet.singe_ec2_public_subnet.id ]
+  subnets = [ aws_subnet.auto_scaling_subnet_1a.id, aws_subnet.auto_scaling_subnet_1b.id ]
   security_groups = [ aws_security_group.default_webserver_security_group.id ]
 }
