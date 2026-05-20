e9b85221a5363f0eafa5e830856ad32a377f5a37
Shoban Chiddarth <messageshoban@gmail.com>
2026-05-20 20:38:23 +0530

    Added listener to alb

--- aws-infrastructure/alb.tf
+++ aws-infrastructure/alb.tf
@@ -5,3 +5,14 @@ resource "aws_alb" "webserver_alb" {
   subnets = [ aws_subnet.auto_scaling_subnet_1a.id, aws_subnet.auto_scaling_subnet_1b.id ]
   security_groups = [ aws_security_group.default_webserver_security_group.id ]
 }
+
+resource "aws_lb_listener" "listener_for_alb" {
+  load_balancer_arn = aws_alb.webserver_alb.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.webservers_tg.arn
+  }
+}
--- aws-infrastructure/target-group.tf
+++ aws-infrastructure/target-group.tf
@@ -3,5 +3,5 @@ resource "aws_lb_target_group" "webservers_tg" {
   port = 80
   protocol = "HTTP"
   vpc_id = aws_vpc.auto_scaling_demo_vpc.id
-  
+
 }
