b56b6646e5c752d1e0ceb0093fc97d08fa80a738
Shoban Chiddarth <messageshoban@gmail.com>
2026-05-20 11:13:31 +0530

    Created launch template for auto scaling group

---
+++ aws-infrastructure/launch-template.tf
@@ -0,0 +1,19 @@
+locals {
+  webserver_autoscaling_init = <<-EOF
+  #!/bin/bash
+  apt-get update
+  apt-get upgrade -y
+  apt-get install nginx -y
+  systemctl enable --now nginx
+  echo "<html><body><h1>This is auto scaling server</h1></body></html>" > /var/www/html
+  EOF
+}
+
+resource "aws_launch_template" "webserver_launch_template" {
+  image_id = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name = aws_key_pair.webserver_public_key.key_name
+  vpc_security_group_ids = [ aws_security_group.default_webserver_security_group.id ]
+  user_data = local.webserver_autoscaling_init
+  update_default_version = true
+}
