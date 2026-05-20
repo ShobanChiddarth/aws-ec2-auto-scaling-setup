661ff3029027265b153298826d190683d6424915
Shoban Chiddarth <messageshoban@gmail.com>
2026-05-20 13:09:07 +0530

    Created 2 private subnets along with route tables

--- aws-infrastructure/rts.tf
+++ aws-infrastructure/rts.tf
@@ -12,3 +12,16 @@ resource "aws_route_table" "auto-scaling-demo-public-rt" {
     "Name" = "auto-scaling-demo-public-rt"
   }
 }
+
+resource "aws_route_table" "private_to_nat_gw" {
+  vpc_id = aws_vpc.auto_scaling_demo_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.autoscaling_nat_gateway.id
+  }
+
+  tags = {
+    "Name" = "route to nat gateway"
+  }
+}
--- aws-infrastructure/subnets.tf
+++ aws-infrastructure/subnets.tf
@@ -12,3 +12,33 @@ resource "aws_route_table_association" "rt-assoc-single-ec2-subnet-public" {
   subnet_id = aws_subnet.singe-ec2-public-subnet.id
   route_table_id = aws_route_table.auto-scaling-demo-public-rt.id
 }
+
+resource "aws_subnet" "auto-scaling-subnet-1a" {
+  availability_zone = "ap-south-1a"
+  cidr_block = "10.0.1.0/24"
+  vpc_id = aws_vpc.auto_scaling_demo_vpc.id
+
+  tags = {
+    "Name" = "auto-scaling-subnet-1a"
+  }
+}
+
+resource "aws_subnet" "auto-scaling-subnet-1b" {
+  availability_zone = "ap-south-1b"
+  cidr_block = "10.0.2.0/24"
+  vpc_id = aws_vpc.auto_scaling_demo_vpc.id
+
+  tags = {
+    "Name" = "auto-scaling-subnet-1b"
+  }
+}
+
+resource "aws_route_table_association" "1a-to-nat" {
+  subnet_id = aws_subnet.auto-scaling-subnet-1a.id
+  route_table_id = aws_route_table.private_to_nat_gw.id
+}
+
+resource "aws_route_table_association" "1b-to-nat" {
+  subnet_id = aws_subnet.auto-scaling-subnet-1b.id
+  route_table_id = aws_route_table.private_to_nat_gw.id
+}
